222ab88e2d4b5ce57960ce74c520921d1fa5b36b
Tung170902 <70988911+Tung170902@users.noreply.github.com>
2022-06-10 09:20:42 +0700

    a

---
+++ src/main/java/com/example/testcsw/entity/UpdateEmployee.java
@@ -0,0 +1,16 @@
+package com.example.testcsw.entity;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class UpdateEmployee {
+   private int id ;
+   private String name;
+   private double salary ;
+
+}
